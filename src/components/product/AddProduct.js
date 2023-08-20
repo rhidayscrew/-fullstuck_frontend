b08293652d74797a23f2733fffc6ea518a244f3c
rhidayscrew17 <rhidayscrew@gmail.com>
2023-08-20 07:44:11 -0400

    update revisi product validasi

---
+++ src/components/product/AddProduct.js
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [token, setToken] = useState("");
+  const [expire, setExpire] = useState("");
+
+  const [title, setTitle] = useState("");
+  const [harbel, setHarbel] = useState("");
+  const [harjul, setHarjul] = useState("");
+  const [stok, setStok] = useState("");
+  //const [makeid, setMakeid] = useState("");
+  const [file, setFile] = useState("");
+  const [preview, setPreview] = useState("");
+  const [messagee, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const loadImage = (e) => {
+    const image = e.target.files[0];
+    setFile(image);
+    setPreview(URL.createObjectURL(image));
+  };
+
+  useEffect(() => {
+    refreshToken();
+    saveProduct();
+
+    //hanya di tambahkan enter doang wkwkw dan tulisan di bawah ini -_- dan ini <a href="# "
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const refreshToken = async () => {
+    try {
+      const response = await axios.get("http://localhost:5000/token");
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+      setName(decoded.name);
+      setExpire(decoded.exp);
+    } catch (error) {
+      if (error.response) {
+        navigate("/");
+      }
+    }
+  };
+
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        const response = await axios.get("http://localhost:5000/token");
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+        const decoded = jwt_decode(response.data.accessToken);
+        setName(decoded.name);
+        setExpire(decoded.exp);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const saveProduct = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("harbel", harbel);
+    formData.append("harjul", harjul);
+    formData.append("stok", stok);
+    try {
+      await axiosJWT.post("http://localhost:5000/products", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      navigate("/productlist");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.message);
+      }
+    }
+  };
+
+  return (
+    <div className="columns is-centered mt-5">
+      <h1>Welcome Back: {name}</h1>
+      <div className="column is-half">
+        <form onSubmit={saveProduct}>
+          <p className="has-text-centered">{messagee}</p>
+
+          <div className="field">
+            <label className="label">Nama barang</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Nama Barang"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Harga beli</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={harbel}
+                onChange={(e) => setHarbel(e.target.value)}
+                placeholder="Harga beli"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Harga jual</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={harjul}
+                onChange={(e) => setHarjul(e.target.value)}
+                placeholder="Harga Jual"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Stok Barang</label>
+            <div className="control">
+              <input
+                type="text"
+                className="input"
+                value={stok}
+                onChange={(e) => setStok(e.target.value)}
+                placeholder="Harga Jual"
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Image</label>
+            <div className="control">
+              <div className="file">
+                <label className="file-label">
+                  <input
+                    type="file"
+                    className="file-input"
+                    onChange={loadImage}
+                  />
+                  <span className="file-cta">
+                    <span className="file-label">Choose a file...</span>
+                  </span>
+                </label>
+              </div>
+            </div>
+          </div>
+
+          {preview ? (
+            <figure className="image is-128x128">
+              <img src={preview} alt="" />
+            </figure>
+          ) : (
+            ""
+          )}
+
+          <div className="field">
+            <div className="control">
+              <button type="submit" className="button is-success">
+                Save
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddProduct;
